0e98ab60e81fc3f098d057c5db9351a41553fcc3
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-11-15 09:32:28 +0530

    CN

--- GoBackNSimple.java
+++ GoBackNSimple.java
@@ -1,12 +1,9 @@
-// Aim: Write a program to implement Sliding Window Protocol for Go-Back-N.
-
 import java.util.*;
 
 class GoBackNSimple {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input total frames and window size
         System.out.print("Enter total number of frames: ");
         int totalFrames = sc.nextInt();
 
@@ -14,25 +11,24 @@ class GoBackNSimple {
         int windowSize = sc.nextInt();
 
         int sent = 0;
+        int lastAck = 0;   // store last ACK
 
-        // Sliding Window Transmission
         while (sent < totalFrames) {
-            // Send frames in the current window
+
+            // send window frames
             for (int i = 0; i < windowSize && sent < totalFrames; i++) {
                 System.out.println("Sent frame: " + (sent + 1));
                 sent++;
             }
 
-            // Simulate ACK reception
             System.out.print("Enter last ACK received (-1 for error): ");
             int ack = sc.nextInt();
 
             if (ack == -1) {
-                // Error: resend from last sent frame
-                System.out.println("Error: Resending from frame " + sent);
+                System.out.println("Error: Resending from frame " + (lastAck + 1));
+                sent = lastAck;   // reset to last acknowledged frame
             } else {
-                // Move window forward
-                sent = ack;
+                lastAck = ack;     // update last ACK
             }
         }
 
